de4cc6b7591ad96fcba9625b4b1e5b6ff3e7fde8
Dani2294 <daniel.agbato@yahoo.fr>
2021-12-21 14:10:57 +0100

    added uglifycss and build task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const uglifycss = require('gulp-uglifycss');
 
 /* Compile SCSS to CSS */
 function buildStyles() {
@@ -11,5 +12,14 @@ function watchChange() {
 	watch(['src/scss/**/*.scss'], buildStyles);
 }
 
+/* Minifying the css */
+function build() {
+	return src('src/css/**/*.css')
+		.pipe(uglifycss({ uglyComments: false }))
+		.pipe(dest('src/dist'));
+}
+
 /* Export and run the two function in order */
 exports.default = series(buildStyles, watchChange);
+
+exports.build = build;
